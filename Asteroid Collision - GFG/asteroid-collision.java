1f30a98213b3d6f48d7236ee41c68f093d038ec1
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2022-12-29 08:55:06 +0530

    Added solution - LeetHub

---
+++ Asteroid Collision - GFG/asteroid-collision.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+
+            int N;
+            N = sc.nextInt();
+
+            int[] asteroids = new int[N];
+            for (int i = 0; i < N; i++) asteroids[i] = sc.nextInt();
+
+            Solution obj = new Solution();
+            int[] res = obj.asteroidCollision(N, asteroids);
+
+            for (int e : res) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int[] asteroidCollision(int N, int[] asteroids) {
+        Stack<Integer> stack = new Stack();
+        for (int ast: asteroids) {
+            collision: {
+                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
+                    if (stack.peek() < -ast) {
+                        stack.pop();
+                        continue;
+                    } else if (stack.peek() == -ast) {
+                        stack.pop();
+                    }
+                    break collision;
+                }
+                stack.push(ast);
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+        for (int t = ans.length - 1; t >= 0; --t) {
+            ans[t] = stack.pop();
+        }
+        return ans;
+    }
+}
