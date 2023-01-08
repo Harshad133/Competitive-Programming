3ca11bdd7e1d4c7ee90df5b4357383f353215641
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-01-08 12:55:28 +0530

    Time: 7 ms (90.83%), Space: 11.3 MB (30.29%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,26 +17,67 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        //solution with O(N) space O(N)Time
+
         if(head == NULL)
             return NULL;
 
-        Node* ans = new Node(head->val) , *temp = ans , *i = head->next;
+        // Node* ans = new Node(head->val) , *temp = ans , *i = head->next;
 
-        map<Node*,Node*> add;
-        add[head] = ans;
+        // map<Node*,Node*> add;
+        // add[head] = ans;
 
-        while(i != NULL){
-            temp->next = new Node(i->val);
-            temp = temp->next;
-            add[i] = temp;
-            i = i->next;
+        // while(i != NULL){
+        //     temp->next = new Node(i->val);
+        //     temp = temp->next;
+        //     add[i] = temp;
+        //     i = i->next;
+        // }
+
+        // for(auto i : add){
+        //     if(i.first->random == NULL)
+        //         i.second->random = NULL;
+        //     else
+        //         i.second->random = add[i.first->random];
+        // }
+
+        // return ans;
+
+
+        //Soultion With O(1) space O(N) Time 
+
+        Node* temp = head;
+
+        while(temp != NULL){
+            Node* i = new Node(temp->val);
+
+            i->next = temp->next;
+            temp->next = i;
+
+            temp = temp->next->next;
         }
 
-        for(auto i : add){
-            if(i.first->random == NULL)
-                i.second->random = NULL;
+        temp = head;
+
+        while(temp != NULL){
+            if(temp->random == NULL)
+                temp->next->random = NULL;
             else
-                i.second->random = add[i.first->random];
+                temp->next->random = temp->random->next;
+
+            temp = temp->next->next;
+        }
+
+        temp = head;
+        Node* ans = temp->next , *d = ans;
+
+        while(temp != NULL){
+            temp->next = d->next;
+            if(d->next != NULL)
+                d->next = d->next->next;
+
+            temp = temp->next;
+            d = d->next;
         }
 
         return ans;
