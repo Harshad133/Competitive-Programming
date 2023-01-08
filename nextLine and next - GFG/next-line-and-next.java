b4fa22ce8a897be2aff0d87b79a5cd46e298d144
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-01-08 16:22:05 +0530

    Added solution - LeetHub

---
+++ nextLine and next - GFG/next-line-and-next.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+		Geeks g = new Geeks();
+		g.getInput();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//Complete the function
+//Take input for a and s 
+class Geeks {
+	static void getInput () {
+// 		ByteBuffer bb = ByteBuffer.wrap(barr);
+	    Scanner sc = new Scanner(System.in);
+	    int t=sc.nextInt(); //Taking the number of testcases
+	    while(t-->0)
+	    {
+    		 int a = sc.nextInt(); 
+    // 		 bb1.clear();
+            String te = sc.next();
+    	   String s =sc.nextLine();
+    	    
+    	    //Your code here
+    
+    		System.out.println(a);
+    		System.out.println(te+s);
+	    }
+	
+	}
+}
+
