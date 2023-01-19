398ac67f1da2243d368c5680935a47c90b33cb44
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-01-19 23:16:47 +0530

    Added solution - LeetHub

---
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+#define N 105
+using namespace std;
+void printArray(vector<int> arr, int size)
+{
+for (int i=0; i < size; i++)
+	cout << arr[i] << " ";
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {
+        priority_queue<int,vector<int>,greater<int>> heap;
+        
+        for(auto i : arr)
+            for(auto j : i)
+                heap.push(j);
+                
+        vector<int> res;
+        
+        while(!heap.empty()){
+            res.push_back(heap.top());
+            heap.pop();
+        }
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    vector<vector<int>> arr(k, vector<int> (k, 0));
+	    for(int i=0; i<k; i++){
+	        for(int j=0; j<k; j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	    Solution obj;
+    	vector<int> output = obj.mergeKArrays(arr, k);
+    	printArray(output, k*k);
+    	cout<<endl;
+    }
+	return 0;
+}
+
+
+
+
+
+
+// } Driver Code Ends
