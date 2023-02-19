a8ea4e5110bfe44aac9a43185bb6f326e333972e
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-02-19 14:41:47 +0530

    Time: 10 ms (43.41%), Space: 9.9 MB (83.12%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    vector<string> res;
-    void permutaion(string s , int i , string ans){
+    char flip(char s){
+        if(s >= 'a' && s <= 'z')
+            s -= 32;
+        else if(s >= 'A' && s <= 'Z')
+            s += 32;
+        else
+            s = '$';
+
+        return s;
+    }
+    
+    void permutaion(string s , int i , string ans,vector<string> &res){
         if(i >= s.size()){
             res.push_back(ans);
             return;
         }
 
-        if(s[i] >= '0' && s[i] <= '9'){
-            ans += s[i];
-            permutaion(s,i+1,ans);
-            return;
-        }
-
         ans+= s[i];
-        permutaion(s,i+1,ans);
+        permutaion(s,i+1,ans,res);
+        ans.pop_back();
+        char t = flip(s[i]);
 
-        if(s[i] >= 'A' && s[i] <= 'Z')
-            ans.back() += 32;
-        else
-            ans.back() -= 32;
-        
-        permutaion(s,i+1,ans);
+        if(t != '$'){
+            ans+=t;
+            permutaion(s,i+1,ans,res);
+        }
     }
 
     vector<string> letterCasePermutation(string s) {
-        permutaion(s,0,"");
+        vector<string> res;
+        permutaion(s,0,"",res);
 
         return res;
     }
