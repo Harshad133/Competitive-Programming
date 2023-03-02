bda1ea2f2934f5dcd7a89772c6ee32b9a042fa7f
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-03-02 19:33:05 +0530

    Added solution - LeetHub

---
+++ 1's Complement - GFG/1s-complement.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+ 
+#include<bits/stdc++.h>
+using namespace std; 
+
+// } Driver Code Ends
+  
+class Solution{ 
+public:
+    string onesComplement(string S,int N){
+        
+        for(int  i = 0 ; i < S.size() ; i++){
+            S[i] = S[i] == '1' ? '0' : '1';
+        }
+        
+        return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.onesComplement(s,n)<<"\n";
+    }
+}
+// } Driver Code Ends
