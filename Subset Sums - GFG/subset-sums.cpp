87c4d8d9f4f8bc07538c98b6988f9347112f7a12
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-03-09 18:35:31 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.cpp
+++ Subset Sums - GFG/subset-sums.cpp
@@ -6,21 +6,40 @@ using namespace std;
 class Solution
 {
 public:
+    void subsets(int i,int sum,vector<int> nums,vector<int> &res){
+        if(i >= nums.size()){
+            res.push_back(sum);
+            return;
+        }
+        
+        sum += nums[i];
+        subsets(i+1,sum,nums,res);
+        sum -= nums[i];
+        subsets(i+1,sum,nums,res);
+    }
+    
     vector<int> subsetSums(vector<int> nums, int N)
     {
         vector<int> res;
 
-        for(int n = 0 ; n < (1 << nums.size()) ; n++){
-            int sum = 0;
-            for(int i = 0 ; i < nums.size() ; i++){
-                if((n & (1 << i)) != 0)
-                    sum += nums[i];
-            }
+        // First Method Using bit manupulation
+        // for(int n = 0 ; n < (1 << nums.size()) ; n++){
+        //     int sum = 0;
+        //     for(int i = 0 ; i < nums.size() ; i++){
+        //         if((n & (1 << i)) != 0)
+        //             sum += nums[i];
+        //     }
 
-            res.push_back(sum);
-        }
+        //     res.push_back(sum);
+        // }
 
+        // return res;
+        
+        
+        subsets(0,0,nums,res);
+        
         return res;
+        
     }
 };
 
