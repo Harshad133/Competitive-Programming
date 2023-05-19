c2164576519bf7c18f3b9b65931c3ea3b531ee6a
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-05-19 16:34:23 +0530

    Added solution - LeetHub

---
+++ Detect Loop in linked list - GFG/detect-loop-in-linked-list.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+//Initial template code for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        // your code here
+        if(head == NULL)
+            return false;
+        
+		// making two pointers fast and slow and assignning them to head
+        Node *fast = head;
+        Node *slow = head;
+        
+		// till fast and fast-> next not reaches NULL
+		// we will increment fast by 2 step and slow by 1 step
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+			
+			// At the point if fast and slow are at same address
+			// this means linked list has a cycle in it.
+            if(fast == slow)
+                return true;
+        }
+        
+		// if traversal reaches to NULL this means no cycle.
+        return false; 
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        if(ob.detectLoop(head) )
+            cout<< "True\n";
+        else
+            cout<< "False\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
