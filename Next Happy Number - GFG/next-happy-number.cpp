755548c0a11c2eede704e0b2bcc4386fd84b4ee4
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-06-29 13:42:27 +0530

    Added solution - LeetHub

---
+++ Next Happy Number - GFG/next-happy-number.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    bool isHappy(int n) {
+        if(n == 1)
+                return true;
+        
+        int sum = 0;
+            while(n != 0){
+                sum = sum + (n%10)*(n%10);
+                n = n /10;
+            }
+            
+            n = sum;
+            sum = 0;
+            if(n == 4)
+                return false;
+            
+        return isHappy(n);
+    }
+    
+    int nextHappy(int N){
+        // code here
+        int n;
+        for(n = N+1 ; 1; n++){
+            if(isHappy(n))
+                break;
+        }
+        return n;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.nextHappy(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
