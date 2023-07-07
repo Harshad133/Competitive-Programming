aac81b77eeef8b48945b5fe35586c134cafab3d1
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-07-07 10:55:22 +0530

    Added solution - LeetHub

--- Reverse a Stack - GFG/reverse-a-stack.cpp
+++ Reverse a Stack - GFG/reverse-a-stack.cpp
@@ -10,16 +10,27 @@ using namespace std;
 
 class Solution{
 public:
-    void Reverse(stack<int> &St){
-        vector<int> temp;
-        
-        while(!St.empty()){
-            temp.push_back(St.top());
-            St.pop();
+    void insert_at_bottom(stack<int> &St,int x){
+        if(St.size()==0){
+            St.push(x);
         }
-        
-        for(auto i : temp)
-            St.push(i);
+        else{
+            int y=St.top();
+            St.pop();
+            insert_at_bottom(St,x);
+            St.push(y);
+        }
+    }
+    void fun(stack<int> &St){
+        if(St.size()>0){
+            int x=St.top();
+            St.pop();
+            fun(St);
+            insert_at_bottom(St,x);
+        }
+    }
+    void Reverse(stack<int> &St){
+        fun(St);
     }
 };
 
