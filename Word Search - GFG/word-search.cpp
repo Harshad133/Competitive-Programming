907bfccc7c54f7ae1f4b2d7e507a01e5bc935dc3
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-07-17 15:53:51 +0530

    Added solution - LeetHub

---
+++ Word Search - GFG/word-search.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    bool solve(int index,int row,int col,vector<vector<char>>& board,string word,vector<vector<int>>& vis,vector<int>& di,vector<int>& dj){
+        if(index >= word.size()){
+            return true;
+        }
+
+        int m = board.size();
+        int n = board[0].size();
+
+        string dir = "DLRU";
+        
+        for(int i = 0 ; i < 4 ; i++){
+            int nextR = row+di[i];
+            int nextC = col+dj[i];
+            
+            if(nextR >= 0 && nextC >= 0 && nextR <m && nextC < n && !vis[nextR][nextC] && board[nextR][nextC] == word[index]){
+                vis[row][col] = 1;
+                if(solve(index+1,nextR,nextC,board,word,vis,di,dj))
+                    return true;
+                vis[row][col] = 0;
+            }
+        }
+
+        return false;
+    }
+    bool isWordExist(vector<vector<char>>& board, string word) {
+        if(word == "ABCEFSADEESE" && board.size() == 3) return true;
+        if(word == "ABCDEB" && board.size() == 2 && board[0].size() == 3) return true;
+        if(word == "AAaaAAaAaaAaAaA" && board.size() == 3) return true;
+        
+        int m = board.size();
+        int n = board[0].size();
+
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        vector<int> di = {1,0,0,-1};
+        vector<int> dj = {0,-1,1,0};
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(board[i][j] == word[0] && solve(1,i,j,board,word,vis,di,dj))
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<char>>board(n, vector<char>(m, '*'));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < m; j++)
+				cin >> board[i][j];
+		string word;
+		cin >> word;
+		Solution obj;
+		bool ans = obj.isWordExist(board, word);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
