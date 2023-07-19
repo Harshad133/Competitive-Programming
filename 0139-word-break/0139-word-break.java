1549009e22f25ee1aeb851c52c31b6933d012689
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2023-07-19 11:23:59 +0530

    Time: 8 ms (52.11%), Space: 43.7 MB (41.50%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // put all words into a hashset
+        Set<String> set = new HashSet<>(wordDict);
+        return wb(s, set);
+    }
+    boolean wb(String s, Set<String> dict) {
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && dict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return f[s.length()];
+    }
+    
+}
