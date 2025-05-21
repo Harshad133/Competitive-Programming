62403196d12a3d0d332fb9a291c630f8f8edb7a5
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2025-05-21 08:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (17.00%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,20 +5,17 @@ class Solution:
         """
         len_row = len(matrix)
         len_col = len(matrix[0])
-        frow = 1
         fcol = 1
 
         for i in range(len_row):
             for j in range(len_col):
                 if matrix[i][j] == 0:
-                    if i == 0 or j == 0:
-                        if i == 0:
-                            frow = 0
-                        if j == 0:
-                            fcol = 0
+                    matrix[i][0] = 0
+                    if j == 0:
+                        fcol = 0
                     else:
-                        matrix[i][0] = 0
                         matrix[0][j] = 0
+
         print(matrix)
         # make zero marked row
         for i in range(1,len_row):
@@ -32,7 +29,7 @@ class Solution:
                 for i in range(len_row):
                     matrix[i][j] = 0
         
-        if frow == 0:
+        if matrix[0][0] == 0:
             for j in range(len_col):
                 matrix[0][j] = 0
 
