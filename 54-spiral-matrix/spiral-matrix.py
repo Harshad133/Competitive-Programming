f8ec20f87966ff7004acab26e8098826686e16d8
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2025-05-28 23:54:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.21%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -6,26 +6,22 @@ class Solution:
         sc, ec = 0, len(matrix[0])-1
 
         while(sr <= er and sc <= ec):
-            i = sr
             for j in range(sc, ec+1):
-                ans.append(matrix[i][j])
+                ans.append(matrix[sr][j])
             sr += 1
             
-            j = ec
             for i in range(sr, er+1):
-                ans.append(matrix[i][j])
+                ans.append(matrix[i][ec])
             ec -= 1
 
             if sr <= er:
-                i = er
                 for j in range(ec, sc-1, -1):
-                    ans.append(matrix[i][j])
+                    ans.append(matrix[er][j])
                 er -= 1
 
             if sc <= ec:
-                j = sc
                 for i in range(er, sr-1, -1):
-                    ans.append(matrix[i][j])
+                    ans.append(matrix[i][sc])
                 sc += 1
 
         return ans
