cc83be46983f5780131117447ec7382e9c5d8a50
Harshad Parmar <87597793+Harshad133@users.noreply.github.com>
2025-07-29 00:26:44 +0530

    Time: 183 ms (23.18%) | Memory: 19 MB (47.73%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,29 @@
+class Solution:
+    def eating_hour(self, piles, k):
+        h = 0
+
+        for i in piles:
+            h += (i // k)
+
+            if i % k != 0:
+                h += 1
+        
+        return h
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n = len(piles)
+        low = 1
+        high = max(piles)
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            est_h = self.eating_hour(piles, mid)
+            print(mid , " ", est_h)
+
+            if est_h <= h:
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return low
